1f3c76ccecafa16d3f32eedb863a0d1b397b578e
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-12-13 16:58:24 +0100

    Created method to fiter students based on multiple choice options

--- SchoolDB/Views/StudentMenu.cs
+++ SchoolDB/Views/StudentMenu.cs
@@ -1,9 +1,54 @@
-﻿namespace SchoolDB.Views;
+﻿using System.Linq.Expressions;
+using SchoolDB.Models;
+using SchoolDB.Options;
+using SchoolDB.Services;
+using static SchoolDB.Options.MenuText;
 
-public class StudentMenu
+namespace SchoolDB.Views;
+
+public static class StudentMenu
 {
+    private static readonly List<List<MenuChoice>> ValidCombinations =
+    [
+        new() { MenuChoice.SortByFirstName, MenuChoice.OrderByDescending },
+        new() { MenuChoice.SortByLastName, MenuChoice.OrderByDescending },
+        new() { MenuChoice.SortByFirstName, MenuChoice.OrderByAscending },
+        new() { MenuChoice.SortByLastName, MenuChoice.OrderByAscending }
+
+    ];
+    
     public static void DisplayStudentMenu()
     {
+        var choice = DisplayUi.DisplayMultiChoiceMenu("Select options to filter by", MenuText.StudentMenuText)
+            .Where(StudentMenuText.ContainsKey)
+            .Select(key => StudentMenuText[key])
+            .ToList();
+
+        var isValidCombination = ValidCombinations.Any(validCombination =>
+            validCombination.SequenceEqual(choice));
         
+        if (isValidCombination)
+        {
+            bool orderBy;
+            
+            Expression<Func<Student, string>> sortBy;
+            
+            if ((int)choice[0] == 4 && (int)choice[1] == 6)
+            {
+                sortBy = s => s.StudentFirstName;
+                orderBy = false;
+            }
+            else
+            {
+                sortBy = s => s.StudentLastName;
+                orderBy = true;
+            }
+
+            Console.WriteLine(StudentRepository.GetStudentsWithClasses(sortBy, orderBy));
+        }
+        else
+        {
+            Console.WriteLine("Invalid combination selected.");
+        }
     }
-}
+}   
