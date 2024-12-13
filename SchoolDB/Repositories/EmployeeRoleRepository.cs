c2973bd1661a630242c90447799a4ed9c70b8770
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-12-13 11:39:13 +0100

    Updated GetEmployeesWithRoles method

--- SchoolDB/Repositories/EmployeeRoleRepository.cs
+++ SchoolDB/Repositories/EmployeeRoleRepository.cs
@@ -10,15 +10,18 @@ public class EmployeeRoleRepository
     {
         using (var context = new SchoolContext())
         {
-           return string.Join("\n", context.EmployeeRoles
-               .GroupBy(er => er.EmployeeIdFkNavigation)
-               .Select(s => new
-               {
-                   EmployeeName = $"{s.Key.EmployeeFirstName} {s.Key.EmployeeLastName}",
-                   RoleName = s.Select(r => r.RoleIdFkNavigation.RoleName)
-               })
-               .ToDictionary(k => k.EmployeeName, v => v.RoleName)
-               .Select(q => $"{q.Key}: {string.Join(", ", q.Value)}"));
+            var query = context.EmployeeRoles
+                .GroupBy(er => er.EmployeeIdFkNavigation)
+                .Select(s => new
+                {
+                    EmployeeName = $"{s.Key.EmployeeFirstName} {s.Key.EmployeeLastName}",
+                    RoleName = s.Select(r => r.RoleIdFkNavigation.RoleName)
+                })
+                .ToDictionary(k => k.EmployeeName, v => v.RoleName);
+
+            var result = string.Join("\n", query.Select(q => $"{q.Key}: {string.Join(", ", q.Value)}"));
+
+            return string.IsNullOrEmpty(result) ? "No employees found." : result;
         }
     }
 }
