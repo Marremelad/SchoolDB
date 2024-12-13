bf41c3da03def7daf8d42cf5970ba20b38911bf6
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-12-13 16:59:43 +0100

    Changed Main Menu text

--- SchoolDB/Options/MenuText.cs
+++ SchoolDB/Options/MenuText.cs
@@ -20,10 +20,10 @@ public static class MenuText
     public static readonly Dictionary<string, MenuChoice> MainMenuText = new()
     {
         // Main Menu Options.
-        { "Show all employees", MenuChoice.Employees },
-        { "Show all admins", MenuChoice.Admins },
-        { "Show all teachers", MenuChoice.Teachers },
-        { "Show all students", MenuChoice.Students }
+        { "Employees", MenuChoice.Employees },
+        { "Admins", MenuChoice.Admins },
+        { "Teachers", MenuChoice.Teachers },
+        { "Students", MenuChoice.Students }
     };
 
     public static readonly Dictionary<string, MenuChoice> StudentMenuText = new()
