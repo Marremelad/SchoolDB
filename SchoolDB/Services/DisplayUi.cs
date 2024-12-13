5ff8138f5e290cf7f439bbc827dcd58513322409
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-12-13 16:57:06 +0100

    Added method to display mulitple choice menu

--- SchoolDB/Services/DisplayUi.cs
+++ SchoolDB/Services/DisplayUi.cs
@@ -1,21 +1,37 @@
-﻿using SchoolDB.Options;
+﻿using SchoolDB.Models;
 using Spectre.Console;
+using static SchoolDB.Options.MenuText;
 
 namespace SchoolDB.Services;
 
 public static class DisplayUi
 {
-    public static MenuText.MenuChoice Display(string title, Dictionary<string, MenuText.MenuChoice> choices)
+    public static MenuChoice DisplaySingleChoiceMenu(string title,
+        Dictionary<string, MenuChoice> choices)
     {
-        Console.WriteLine(title);
-            
         var choice = AnsiConsole.Prompt(
             new SelectionPrompt<string>()
-                .Title("Choose an option.")
+                .Title(title)
                 .PageSize(10)
                 .MoreChoicesText("Move up and down to reveal more options")
                 .AddChoices(choices.Select(s => s.Key)));
 
         return choices[choice];
+    }
+
+    public static List<string> DisplayMultiChoiceMenu(string title,
+        Dictionary<string, MenuChoice> choices)
+    {
+        var multipleChoices = AnsiConsole.Prompt(
+            new MultiSelectionPrompt<string>()
+                .Title(title)
+                .PageSize(10)
+                .MoreChoicesText("[grey](Move up and down to reveal more options)[/]")
+                .InstructionsText(
+                    "[grey](Press [blue]<space>[/] to toggle an option, " + 
+                    "[green]<enter>[/] to accept)[/]")
+                .AddChoices(choices.Select(s => s.Key)));
+
+        return multipleChoices;
     }
 }
