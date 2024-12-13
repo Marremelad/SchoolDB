7d4a01ceeee11597b79759e0a28d6912e89a3989
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-12-13 13:34:34 +0100

    Renamed file

---
+++ SchoolDB/Services/DisplayUi.cs
@@ -0,0 +1,22 @@
+﻿using SchoolDB.Options;
+using Spectre.Console;
+
+namespace SchoolDB.Services;
+
+public static class DisplayMenu
+{
+    public static MenuText.MenuChoice Display(string title, Dictionary<string, MenuText.MenuChoice> choices)
+    {
+        while (true)
+        {
+            var choice = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                    .Title("Choose an option.")
+                    .PageSize(10)
+                    .MoreChoicesText("Move up and down to reveal more options")
+                    .AddChoices(choices.Select(s => s.Key)));
+
+            return choices[choice];
+        }
+    }
+}
