c0bdf901623937a8152008905b59cebacbeb4d51
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-12-14 16:30:21 +0100

    Added method to display grades within the last month

--- SchoolDB/Repositories/CourseEnrolmentRepository.cs
+++ SchoolDB/Repositories/CourseEnrolmentRepository.cs
@@ -1,6 +1,27 @@
-﻿namespace SchoolDB;
+﻿using SchoolDB.Data;
 
-public class CourseEnrolmentRepository
+namespace SchoolDB;
+
+public static class CourseEnrolmentRepository
 {
-    
+    public static string RecentGrades()
+    {
+        using (var context = new SchoolContext())
+        {
+            var thirtyDaysAgo = DateOnly.FromDateTime(DateTime.Now.AddDays(-30));
+
+            var query = context.CourseEnrolments
+                .Where(g => g.GradingDate >= thirtyDaysAgo)
+                .Select(s => 
+                    $"Grade: {s.Grade}, " +
+                    $"Date: {s.GradingDate}, " +
+                    $"Course: {s.CourseIdFkNavigation.CourseName}, " +
+                    $"Student: {s.StudentIdFkNavigation.StudentFirstName} " +
+                    $"{s.StudentIdFkNavigation.StudentLastName}");
+
+            var result = string.Join("\n", query);
+
+            return result;
+        }
+    }
 }
