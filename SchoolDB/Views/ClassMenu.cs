ad78bccb330fd93ce4a6eeda5548a6ff7c921daa
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-12-14 15:53:44 +0100

    Added LINQ to get string key from dictionary where the enum value is equal to the choice

--- SchoolDB/Views/ClassMenu.cs
+++ SchoolDB/Views/ClassMenu.cs
@@ -8,5 +8,9 @@ public static class ClassMenu
     public static void DisplayClassMenu()
     {
         var choice = DisplayUi.DisplaySingleChoiceMenu("Select a class to see it's students", MenuText.ClassMenuText);
+
+        var chosenClass = MenuText.ClassMenuText.First(p => p.Value == choice).Key;
+
+        Console.WriteLine(StudentRepository.GetStudentsFilteredByClass(chosenClass));
     }
 }
