3a3f20db15e6f0792a4472c359aa92bf2da20162
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-12-15 11:48:45 +0100

    Created method to get course statistics

--- SchoolDB/Repositories/CourseEnrolmentRepository.cs
+++ SchoolDB/Repositories/CourseEnrolmentRepository.cs
@@ -4,6 +4,16 @@ namespace SchoolDB;
 
 public static class CourseEnrolmentRepository
 {
+    private static readonly Dictionary<string, int> GradeMapping = new()
+    {
+        { "A", 5 },
+        { "B", 4 },
+        { "C", 3 },
+        { "D", 2 },
+        { "E", 1 },
+        { "F", 0 }
+    };
+    
     public static string GetRecentGrades()
     {
         using (var context = new SchoolContext())
@@ -11,7 +21,7 @@ public static class CourseEnrolmentRepository
             var thirtyDaysAgo = DateOnly.FromDateTime(DateTime.Now.AddDays(-30));
 
             var query = context.CourseEnrolments
-                .Where(g => g.GradingDate >= thirtyDaysAgo)
+                .Where(ce => ce.GradingDate >= thirtyDaysAgo)
                 .Select(s => 
                     $"Grade: {s.Grade}, " +
                     $"Date: {s.GradingDate}, " +
@@ -20,6 +30,68 @@ public static class CourseEnrolmentRepository
                     $"{s.StudentIdFkNavigation.StudentLastName}");
 
             var result = string.Join("\n", query);
+
+            return result;
+        }
+    }
+
+    public static string GetCourseStats(string course)
+    {
+        using (var context = new SchoolContext())
+        {
+            var query = context.CourseEnrolments
+                .Where(ce => ce.CourseIdFkNavigation.CourseName == course)
+                .Select(s => $"{s.Grade}");
+            
+            var numericGrades = new List<int>();
+            
+            foreach (var grade in query)
+            {
+                if (grade == "") continue;
+                numericGrades.Add(GradeMapping[grade]);
+            }
+
+            if (numericGrades.Count == 0) return "No available data for this course." +
+q            
+            var numericHighestGrade = numericGrades.Max();
+            var numericLowestGrade = numericGrades.Min();
+            var numericAverageGrade = Convert.ToInt32(numericGrades.Average());
+
+           return StringOfCourseStats(course, numericHighestGrade, numericLowestGrade, numericAverageGrade);
+        }
+    }
+
+    private static string StringOfCourseStats(string course, int numericHighestGrade, int numericLowestGrade, int numericAverageGrade)
+    {
+        var highestGrade = GradeMapping.First(kvp => kvp.Value == numericHighestGrade).Key;
+        var lowestGrade = GradeMapping.First(kvp => kvp.Value == numericLowestGrade).Key;
+        var averageGrade = GradeMapping.First(kvp => kvp.Value == numericAverageGrade).Key;
+        
+        using (var context = new SchoolContext())
+        {
+            var queryForHighestGrade = context.CourseEnrolments
+                .Where(ce => ce.CourseIdFkNavigation.CourseName == course && ce.Grade == highestGrade)
+                .Select(s => 
+                    $"{s.StudentIdFkNavigation.StudentFirstName} " +
+                    $"{s.StudentIdFkNavigation.StudentLastName}")
+                .ToList();
+
+            var queryForLowestGrade = context.CourseEnrolments
+                .Where(ce => ce.CourseIdFkNavigation.CourseName == course && ce.Grade == lowestGrade)
+                .Select(s =>
+                    $"{s.StudentIdFkNavigation.StudentFirstName} " +
+                    $"{s.StudentIdFkNavigation.StudentLastName}")
+                .ToList();
+
+            var result = string.Join("\n", new[] 
+            {
+                $"Course: {course}",
+                $"\nAverage Grade: {averageGrade}",
+                $"\nHighest Grade: {highestGrade}: Acquired by -",
+                string.Join("\n", queryForHighestGrade),
+                $"\nLowest Grade: {lowestGrade}: Acquired by -",
+                string.Join("\n", queryForLowestGrade)
+            });
 
             return result;
         }
